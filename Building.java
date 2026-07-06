afd893b4374d9478320dc6ddd6de9f6d2888b759
Desto10 <164202149+Desto10@users.noreply.github.com>
2026-07-06 16:43:01 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -1,9 +1,11 @@
 package com.hccs;
 import java.util.Scanner;
+// parent class, has what every building should have
 public class Building {
+	// private attributes 
 private String siding;
 private double squareFeet;
-
+// getters and setter 
 public String getSiding() {
 	return siding;
 }
@@ -19,16 +21,16 @@ public void setsquareFeet(double SquareFeet) {
 	this.squareFeet = SquareFeet;
 	
 }
-
+// main method which creates one house and one commercial building 
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
-	
+	// get house info from the user 
 	House myHouse = new House();
 	myHouse.getInput(input);
-	
+	// get commerical info from the user 
 	Commercial myCommercial = new Commercial();
 	myCommercial.getInput(input);
-	
+	// showing the totals 
 	System.out.println(" RESULTS");
 	System.out.println("House total square feet:" + myHouse.getSquareFeet());
 	System.out.println("Commercial total square feet:" + myCommercial.getSquareFeet());
